358a0cc2fb8ffd8ba3f600113fc9208a630d8bfa
Hossein <hossein.aghili.2658@gmail.com>
2025-11-07 21:47:42 +0330

    create function task controller

--- api/Controllers/task.controller.js
+++ api/Controllers/task.controller.js
@@ -1,4 +1,4 @@
-import ApiFeatures, { catchAsync } from "vanta-api";
+import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
 import Task from "../Models/task.model.js";
 
 export const create = catchAsync(async (req, res, next) => {
@@ -21,8 +21,22 @@ export const getAll = catchAsync(async (req, res, next) => {
     const user = await features.execute()
     const count = await Task.countDocuments(features.queryFilter)
     return res.status(200).json({
-        success:true,
-        data:user,
+        success: true,
+        data: user,
         count,
+    })
+})
+export const getOne = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    const task = await Task.findById(id)
+    if (!task) {
+        return next(new HandleERROR('تسک مورد نظر یافت نشد'))
+    }
+    if (req.role !== "admin" && task.userId.toString() !== req.userId.toString()) {
+        return next(new HandleERROR("شما اجازه دسترسی به این را ندارید", 401))
+    }
+    return res.status(200).json({
+        success: true,
+        data: user,
     })
 })
