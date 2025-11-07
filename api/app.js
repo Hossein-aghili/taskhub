7983dd7394ca79ed9793646b3d93c5bd1ab9924f
Hossein <hossein.aghili.2658@gmail.com>
2025-11-07 07:45:44 +0330

    create function app and create middlewares

--- api/app.js
+++ api/app.js
@@ -1,7 +1,16 @@
 import express from 'express'
-
+import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import morgan from 'morgan'
 
 const __filename =fileURLToPath(import.meta.url)
 export const __dirname = path.dirname(__filename)
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+
+export default app
