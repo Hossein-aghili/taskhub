2e40a056f1eef58b284cf3760c6420ac9b246f49
Hossein <hossein.aghili.2658@gmail.com>
2025-11-08 11:04:17 +0330

    Debug user

--- api/Controllers/user.controller.js
+++ api/Controllers/user.controller.js
@@ -1,46 +1,41 @@
-import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
+import ApiFeatures, { catchAsync } from "vanta-api";
 import User from "../Models/user.model.js";
 
 export const getAll = catchAsync(async (req, res, next) => {
     const features = new ApiFeatures(User, req.query, req.role)
         .filter()
         .populate()
-        .paginate()
         .limitFields()
+        .paginate()
         .sort()
     const users = await features.execute()
-    const count = await User.countDocuments(req.query)
+    const count = await User.countDocuments(features.queryFilter)
     return res.status(200).json({
         success: true,
         data: users,
-        count,
-        message: 'users successfully'
+        count
     })
 })
 export const getOne = catchAsync(async (req, res, next) => {
     const { id } = req.params
     if (req.role !== 'admin' && req.userId !== id) {
-        return next(new HandleERROR('you dont have permission'), 401)
+        return next(new HandleERROR("you don't have permission", 401))
     }
     const user = await User.findById(id)
-    if (!user) {
-        return next(new HandleERROR('user not fund'), 404)
-    }
     return res.status(200).json({
         success: true,
         data: user,
-        message: 'user successfully'
     })
 })
 export const update = catchAsync(async (req, res, next) => {
     const { id } = req.params
     if (req.role !== 'admin' && req.userId !== id) {
-        return next(new HandleERROR('you dont have permission'), 401)
+        return next(new HandleERROR("you don't have permission", 401))
     }
-    let user
+    let user;
     if (req.role === 'admin') {
         const { password = null, ...others } = req.body
-        user = await User.findByIdAndUpdate(id, others, {
+        user = await User.findByIdAndUpdate(id, o, {
             new: true,
             runValidators: true
         })
@@ -51,9 +46,8 @@ export const update = catchAsync(async (req, res, next) => {
             runValidators: true
         })
     }
-    return res.status(200).json(({
+    return res.status(200).json({
         success: true,
-        data: user,
-        message: 'update successfully'
-    }))
+        data: user
+    })
 })
