8f21e3735454038a6778d230b1c8dc50e0bf82cf
Hossein <hossein.aghili.2658@gmail.com>
2025-11-08 11:04:58 +0330

    Debug task

--- api/Controllers/task.controller.js
+++ api/Controllers/task.controller.js
@@ -1,4 +1,5 @@
-import ApiFeatures, { catchAsync, HandleERROR } from "vanta-api";
+import ApiFeatures, { HandleERROR } from "vanta-api";
+import { catchAsync } from 'vanta-api'
 import Task from "../Models/task.model.js";
 
 export const create = catchAsync(async (req, res, next) => {
@@ -11,18 +12,21 @@ export const create = catchAsync(async (req, res, next) => {
 })
 
 export const getAll = catchAsync(async (req, res, next) => {
-    const features = new ApiFeatures(Task, req.query, req.role)
+    const filterObj = req.role !== "admin" ? { userId: req.userId } : {};
+    const features = new ApiFeatures(Task, req.query)
+        .addManualFilters(filterObj)
         .filter()
-        .addManualFilters(req.role !== 'andim' ? { userId: req.userId } : {})
         .sort()
         .populate()
         .paginate()
         .limitFields()
-    const user = await features.execute()
+
+    const tasks = await features.execute()
     const count = await Task.countDocuments(features.queryFilter)
+
     return res.status(200).json({
         success: true,
-        data: user,
+        data: tasks,
         count,
     })
 })
@@ -37,24 +41,25 @@ export const getOne = catchAsync(async (req, res, next) => {
     }
     return res.status(200).json({
         success: true,
-        data: user,
+        data: task,
     })
 })
 
 export const update = catchAsync(async (req, res, next) => {
     const { id } = req.params
-    if (req.role !== "admin" && task.userId.toString() !== req.userId.toString()) {
-        return next(new HandleERROR("شما اجازه دسترسی به این را ندارید", 401))
-    }
+    
     const task = await Task.findById(id)
     if (!task) {
         return next(new HandleERROR('تسک مورد نظر یافت نشد'))
+    }
+    if (req.role !== "admin" && task.userId.toString() !== req.userId.toString()) {
+        return next(new HandleERROR("شما اجازه دسترسی به این را ندارید", 401))
     }
     const updateTask = await Task.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
 
     return res.status(200).json({
         success: true,
-        data: user,
+        data: updateTask,
     })
 })
 export const remove = catchAsync(async (req, res, next) => {
