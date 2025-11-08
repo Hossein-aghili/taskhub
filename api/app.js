a6735af72045e7e98bb4d2a6c11733e27a450b6c
Hossein <hossein.aghili.2658@gmail.com>
2025-11-08 10:51:05 +0330

    add use middlewares route auth and user

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import morgan from 'morgan'
 import taskRouter from './Routes/task.route.js'
+import authRouter from './Routes/auth.route.js'
+import userRouter from './Routes/user.route.js'
 
 const __filename =fileURLToPath(import.meta.url)
 export const __dirname = path.dirname(__filename)
@@ -12,6 +14,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
-app.use('/api/task',taskRouter)
+app.use('/api/task', taskRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 export default app
